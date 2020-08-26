1d1888c126879c3eaa37130d7e294f145a3cf57b
Chris Lowe <clowe@luc.edu>
2020-08-25 21:55:01 -0600

    Initial commit

---
+++ GetCapacity/GetCapacity.cs
@@ -0,0 +1,156 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Linq;
+using Jurassic.Library;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace GetCapacity
+{
+  public static class GetCapacity
+  {
+    [FunctionName("GetCapacity")]
+    public static void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+    {
+      log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now.TimeOfDay}");
+
+      //if (!ShouldRun()) return;
+
+      try
+      {
+        IConfigurationRoot config = GetConfig(log, context);
+
+        string url = config["url"];
+        string xpath = config["xpath"];
+
+        DayOfWeek lastUpdate = (DayOfWeek)Enum.Parse(typeof(DayOfWeek),
+        (config["lastUpdateDay"] != null) ? config["lastUpdateDay"] : DateTime.Now.DayOfWeek.ToString()); //TODO:
+
+        //TODO: Consider timezone
+        if (DateTime.Now.DayOfWeek.Equals(DayOfWeek.Sunday) && lastUpdate.Equals(DayOfWeek.Saturday))
+        {
+          //TODO: Create a new file and rename the old file
+        }
+
+        HtmlWeb web = new HtmlWeb();
+        var htmlDoc = web.Load(url);
+
+        var script = htmlDoc.DocumentNode.Descendants()
+                        .Where(n => n.Name == "script"
+                            && n.XPath.Equals(xpath))
+                        .First().InnerText;
+
+        string scriptJson = script.Substring(0, script.IndexOf("function"));
+
+        var engine = new Jurassic.ScriptEngine();
+        var result = engine.Evaluate("(function() { " + scriptJson + " return data; })()");
+        var capacityData = JSONObject.Stringify(engine, result).Replace("\\", string.Empty);
+
+        string timestampUtc = DateTime.Now.ToString();
+        string json = JsonConvert.SerializeObject(capacityData);
+
+        CloudBlockBlob blob = GetBlobConnection(log, config);
+
+        //download current json
+        string existingData = blob.DownloadTextAsync(System.Text.Encoding.ASCII, null, new BlobRequestOptions { DisableContentMD5Validation = true }, new OperationContext()).Result;
+        int existingDataLength = existingData.Length;
+        log.LogInformation($"Existing data size {existingDataLength}");
+
+        StringBuilder sb = new StringBuilder(existingData);
+        if (sb.Length > 0)
+        {
+          sb.Remove(sb.Length - 1, 1); // remove ending }
+          sb.Append(",");
+        }
+        else
+        {
+          sb.Append("{");
+        }
+
+        sb.Append($"\"{timestampUtc}\": {json}");
+        sb.Append("}");
+
+        blob.UploadTextAsync(sb.ToString());
+        log.LogInformation("Done!");
+      }
+      catch (Exception ex)
+      {
+        log.LogInformation($"Problem: {ex}");
+      }
+    }
+
+    public static CloudBlockBlob GetBlobConnection(ILogger log, IConfigurationRoot config)
+    {
+      CloudBlockBlob retval = null;
+      try
+      {
+        string blobConnectionString = config["BlobConnectionString"];
+        string containerName = config["BlobContainerName"];
+
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(blobConnectionString);
+        CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+        CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+        retval = container.GetBlockBlobReference("data.json");
+      }
+      catch (Exception ex)
+      {
+        log.LogCritical($"Error: {ex}. {ex.Message}");
+      }
+      return retval;
+    }
+
+    public static bool ShouldRun()
+    {
+      // TODO: Figure out the span of time for all gyms
+      TimeSpan startTime = new TimeSpan(5, 0, 0);
+      TimeSpan endTime = new TimeSpan(21, 30, 0);
+      DateTime now = DateTime.Now;
+
+      if (endTime == startTime)
+      {
+        return true;
+      }
+      else if (endTime < startTime)
+      {
+        return now.TimeOfDay <= endTime ||
+            now.TimeOfDay >= startTime;
+      }
+      else
+      {
+        return now.TimeOfDay >= startTime &&
+            now.TimeOfDay <= endTime;
+      }
+    }
+
+    public static IConfigurationRoot GetConfig(ILogger log, ExecutionContext context)
+    {
+      try
+      {
+        log.LogInformation("Building out the config root");
+        IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddEnvironmentVariables();
+
+        IConfigurationRoot config = configBuilder.Build();
+
+        configBuilder.AddAzureKeyVault(
+            $"https://{config["AzureKeyVault:VaultName"]}.vault.azure.net/",
+            config["AzureKeyVault:ClientId"],
+            config["AzureKeyVault:ClientSecret"]
+        );
+        return configBuilder.Build();
+      }
+      catch (Exception ex)
+      {
+        log.LogCritical(ex.ToString());
+        throw;
+      }
+    }
+  }
+}
---
+++ deploy/deployInfra.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+CONFIGFILE="./config"
+
+eval $(sed '/:/!d;/^ *#/d;s/:/ /;' < "$CONFIGFILE" | while read -r key val
+do
+    str="$key='$val'"
+    echo "$str"
+done)
+
+dt=$(date '+%Y%m%d%H%M');
+
+resourceGroupName="capacity"$dt
+storageAccountName="blob"$dt
+fnStorageAccountName="fnsto"$dt
+functionAppName="getcapacity"$dt
+functionContainer="function"$dt
+keyvaultName="kv"$dt
+blobName="blob"$dt
+containerName="container"$dt
+servicePrincipalName="sp"$dt
+
+echo "Creating Azure resources..."
+
+az login --service-principal -u $appID --password $password --tenant $tenant
+
+az account set --subscription $subscription
+
+echo "Creating resource group" $resourceGroupName"..."
+az group create --name $resourceGroupName --location $location
+
+echo "Creating key vault" $keyvaultName"..."
+az keyvault create \
+  --resource-group $resourceGroupName \
+  --name $keyvaultName \
+  --location $location \
+  --sku "standard" \
+  --subscription $subscription
+
+echo "Creating storage account" $storageAccountName"..."
+az storage account create  \
+  --name $storageAccountName  \
+  --resource-group $resourceGroupName \
+  --access-tier "Cool" \
+  --kind "BlobStorage" \
+  --location $location \
+  --sku "Standard_LRS" \
+  --subscription $subscription
+
+echo "Retrieving connection string..."
+connectionString=`az storage account show-connection-string --name $storageAccountName --resource-group $resourceGroupName --output tsv`
+
+echo "Populating key vault..."
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "Url" \
+  --value $url
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "XPath" \
+  --value $xpath
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "BlobContainerName" \
+  --value $containerName
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "StorageAccountName" \
+  --value $blobName
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "ResourceGroupName" \
+  --value $resourceGroupName
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "BlobConnectionString" \
+  --value $connectionString
+
+az keyvault secret set \
+  --vault-name $keyvaultName \
+  --name "ContainerName" \
+  --value $containerName
+
+echo "Creating storage container" $containerName"..."
+az storage container create \
+  --name $containerName \
+  --public-access off \
+  --connection-string $connectionString
+
+echo "Uploading to blob storage" $blobName"..."
+az storage blob upload --container-name $containerName \
+  --file $pathToFile"data.json" \
+  --connection-string $connectionString \
+  --name "data.json"
+
+echo "Creating function app" $functionAppName
+az storage account create \
+  --name $fnStorageAccountName \
+  --resource-group $resourceGroupName \
+  --location $location \
+  --sku Standard_LRS \
+  --subscription $subscription
+
+az functionapp create \
+  --name $functionAppName  \
+  --storage-account $fnStorageAccountName  \
+  --consumption-plan-location $location \
+  --functions-version 2 \
+  --resource-group $resourceGroupName 
+
+echo "Building and publishing the function project" $functionProjectLocation"/GetCapacity.csproj..."
+dotnet build $functionProjectLocation --configuration Release
+cd $functionProjectLocation"/bin/Release/netcoreapp3.1/"
+zip -r $functionAppName.zip * 
+
+az storage container create \
+  --name $functionContainer \
+  --public-access off \
+  --connection-string $connectionString
+
+az storage container create \
+  --name $functionContainer \
+  --public-access off \
+  --connection-string $connectionString
+
+echo "Uploading function zip to blob"
+az storage blob upload --container-name $functionContainer \
+  --file $functionAppName.zip \
+  --connection-string $connectionString \
+  --name $functionAppName.zip
+
+format='+%Y-%m-%dT%H:%M:%SZ'
+start=`date -v-24H ${format}`
+end=`date -v+8760H ${format}`
+
+sas=`az storage account generate-sas \
+  --permissions rl \
+  --connection-string $connectionString \
+  --expiry $end \
+  --start $start \
+  --resource-types sco \
+  --services b \
+  --output tsv`
+sas="${sas//%3A/:}"
+  
+echo "Cleaning up "${functionAppName}".zip..."
+rm ${functionAppName}.zip
+
+packageLocation="https://$storageAccountName.blob.core.windows.net/$functionContainer/$functionAppName.zip?$sas"
+
+az functionapp config appsettings set \
+  --name $functionAppName \
+  --resource-group $resourceGroupName \
+  --settings AzureKeyVault:ClientId=$appID AzureKeyVault:ClientSecret=$password AzureKeyVault:VaultName=$keyvaultName WEBSITE_RUN_FROM_PACKAGE=$packageLocation
+
+echo "Done!"
